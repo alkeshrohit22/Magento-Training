fc0e5f496591eb0488fe89111cf0c6213bf54e0d
alkeshrohit22 <alkesh.rohit@sigmainfo.net>
2023-05-11 19:18:50 +0530

    custom product attribute created

---
+++ app/code/My/ClothingMaterial/Model/Attribute/Backend/Material.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace My\ClothingMaterial\Model\Attribute\Backend;
+
+use Magento\Eav\Model\Entity\Attribute\Backend\AbstractBackend;
+
+class Material extends AbstractBackend
+{
+    /**
+     * Validate
+     * @param \Magento\Catalog\Model\Product $object
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @return bool
+     */
+    public function validate($object)
+    {
+        $value = $object->getData($this->getAttribute()->getAttributeCode());
+        if ( ($object->getAttributeSetId() == 10) && ($value == 'wool')) {
+            throw new \Magento\Framework\Exception\LocalizedException(
+                __('Bottom can not be wool.')
+            );
+        }
+        return true;
+    }
+}
---
+++ app/code/My/ClothingMaterial/Model/Attribute/Frontend/Material.php
@@ -0,0 +1,14 @@
+<?php
+namespace My\ClothingMaterial\Model\Attribute\Frontend;
+
+use Magento\Eav\Model\Entity\Attribute\Frontend\AbstractFrontend;
+use Magento\Framework\DataObject;
+
+class Material extends AbstractFrontend
+{
+    public function getValue(DataObject $object)
+    {
+        $value = $object->getData($this->getAttribute()->getAttributeCode());
+        return "<b>$value</b>";
+    }
+}
